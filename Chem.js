16f6f41b75681e10ce1d46822310cfbc7fc51bb6
stett <stettjawa@gmail.com>
2015-01-31 00:46:52 -0800

    no more matterjs?

--- Chem.js
+++ Chem.js
@@ -1,29 +1,36 @@
+/*
 var World = Matter.World,
             Bodies = Matter.Bodies;
+*/
 
 var Chem = {
-    load: function(cid, world, x, y) {
-
-        // Create a physics body corresponding to an atom
-        var instantiate_atom = function(atom) {
-            var scale = 20;
-            var body = Bodies.circle(
-                atom.coords.x*scale + x, atom.coords.y*scale + y, 2);
-            World.add(world, body);
-            atom['body'] = body;
-            return atom;
-        }
+    //load: function(cid, world, x, y) {
+    load_molecule: function(cid, x, y, success) {
 
         // Create a "covalent bond" between two atoms
         var bond_atoms = function(atom_0, atom_1) {
 
-            // TODO: Do stuff
+            // Get the bond distance
+            var dx = atom_1.coords.x - atom_0.coords.x;
+            var dy = atom_1.coords.y - atom_0.coords.y;
+            var dist = Math.sqrt(dx*dx + dy*dy);
+
+            // Store the bond in the one with the lower id
+            if (atom_0.id < atom_1.id) {
+                atom_0.bonds.push({
+                    'id': atom_1.id,
+                    'dist': dist});
+            } else {
+                atom_1.bonds.push({
+                    'id': atom_0.id,
+                    'dist': dist});
+            }
         }
 
         // Using compound structural data, generate the physical object
         var instantiate_molecule = function(compound) {
             console.log("Instantiating compound");
-            console.log(compound);
+            //console.log(compound);
 
             // Instantiate all atoms and save them in a dict
             var atoms = {}
@@ -33,13 +40,21 @@ var Chem = {
             var atom_coords_y = compound.coords[0].conformers[0].y;
             for (var i = 0; i < atom_ids.length; i++) {
                 atom = {
+                    'id': atom_ids[i],
                     'element': atom_elements[i],
+                    'bonds': [],
                     'coords': {
                         'x': atom_coords_x[i],
-                        'y': atom_coords_y[i],
-                    }
+                        'y': atom_coords_y[i]},
+                    'vel': {
+                        'x': 0,
+                        'y': 0},
+                    'accel': {
+                        'x': 0,
+                        'y': 0},
                 };
-                atoms[atom_ids[i]] = instantiate_atom(atom);
+                //atoms[atom_ids[i]] = instantiate_atom(atom);
+                atoms[atom_ids[i]] = atom;
             };
 
             // Create covalent (spring) bonds between atoms
@@ -49,11 +64,18 @@ var Chem = {
             for (var i = 0; i < atom_bond_1_ids.length; i++) {
                 var id1 = atom_bond_1_ids[i];
                 var id2 = atom_bond_2_ids[i];
-                bond_atoms(atoms[id1], atoms[id2]);
+                var bond = bond_atoms(atoms[id1], atoms[id2]);
             }
 
+
             // Return the collection of atoms
-            return atoms;
+            return {
+                'atoms': atoms,
+                'coords': {
+                    'x': x,
+                    'y': y,
+                }
+            };
         };
 
         // Make an AJAX call to the pubchem database
@@ -67,7 +89,14 @@ var Chem = {
             },
             success: function(data, textStatus, jqXHR) {
                 console.log("Compound Loaded (CID: " + cid + ")");
-                atoms = instantiate_molecule(data.PC_Compounds[0]);
+                molecule = instantiate_molecule(data.PC_Compounds[0]);
+
+                // Add the molecule to the molecules array
+                //molecules.push(molecule);
+                //console.log(molecules);
+
+                // Call the success callback function
+                success(molecule);
             }
         });
     },
--- index.js
+++ index.js
@@ -1,3 +1,152 @@
+
+var canvas = $('canvas#mm').get(0);
+var context = canvas.getContext('2d');
+
+context.font = "bold 12px sans-serif";
+
+
+var molecules = []
+Chem.load_molecule(
+    6140, 30, 200,
+    function(molecule) {molecules.push(molecule); }
+);
+/*
+Chem.load_molecule(
+    6141, 300, 100,
+    function(molecule) {molecules.push(molecule); }
+);
+*/
+
+var update = function(dt) {
+    for (var i in molecules) {
+        var molecule = molecules[i];
+        for (var j in molecule.atoms) {
+            var atom = molecule.atoms[j];
+            atom.accel.x = 0;
+            atom.accel.y = 0;
+        }
+    }
+
+    for (var i in molecules) {
+        var molecule = molecules[i];
+        for (var j in molecule.atoms) {
+            var atom0 = molecule.atoms[j];
+            var x0 = atom0.coords.x;
+            var y0 = atom0.coords.y;
+            for (var k in atom0.bonds) {
+                var bond = atom0.bonds[k];
+                var atom1 = molecule.atoms[bond.id];
+                var x1 = atom1.coords.x;
+                var y1 = atom1.coords.y;
+
+                var dx = x1 - x0;
+                var dy = y1 - y0;
+                var dist = Math.sqrt(dx*dx + dy*dy);
+                var nx = dx / dist;
+                var ny = dy / dist;
+                var accel = (dist - bond.dist) * 100;
+
+                atom0.accel.x += nx * accel;
+                atom0.accel.y += ny * accel;
+                atom1.accel.x -= nx * accel;
+                atom1.accel.y -= ny * accel;
+            }
+
+            /*
+            for (var k in molecule.atoms) {
+                if (k <= j) continue;
+                var atom1 = molecule.atoms[k];
+
+                var x1 = atom1.coords.x;
+                var y1 = atom1.coords.y;
+
+                var dx = x1 - x0;
+                var dy = y1 - y0;
+                var dist = Math.sqrt(dx*dx + dy*dy);
+                var nx = dx / dist;
+                var ny = dy / dist;
+                var accel = -dist * dist / 100;
+
+                atom0.accel.x += nx * accel;
+                atom0.accel.y += ny * accel;
+                atom1.accel.x -= nx * accel;
+                atom1.accel.y -= ny * accel;
+            }
+            */
+        }
+    }
+
+    for (var i in molecules) {
+        var molecule = molecules[i];
+        for (var j in molecule.atoms) {
+            var atom = molecule.atoms[j];
+            atom.vel.x += atom.accel.x * dt;
+            atom.vel.y += atom.accel.y * dt;
+            atom.coords.x += atom.vel.x * dt;
+            atom.coords.y += atom.vel.y * dt;
+        }
+    }
+};
+
+var draw = function() {
+
+    // Clears the canvas, and dynamically sizes it to window
+    canvas.width = $('body').width();
+    canvas.height = $('body').height();
+    context.rect(0, 0, canvas.width, canvas.height);
+    context.fillStyle = "#efefef";
+    context.fill();
+
+    // Loop through and draw all atoms
+    for (var i in molecules) {
+        var molecule = molecules[i];
+
+        for (var j in molecule.atoms) {
+            var atom = molecule.atoms[j];
+
+            var x = molecule.coords.x + atom.coords.x * 40;
+            var y = molecule.coords.y + atom.coords.y * 40;
+
+            for (var k in atom.bonds) {
+                var bond = atom.bonds[k];
+                var atom1 = molecule.atoms[bond.id];
+                var x1 = molecule.coords.x + atom1.coords.x * 40;
+                var y1 = molecule.coords.y + atom1.coords.y * 40;
+                context.beginPath();
+                context.moveTo(x, y);
+                context.lineTo(x1, y1);
+                context.lineWidth = 4;
+                context.strokeStyle = '#777';
+                context.stroke();
+            }
+
+            context.beginPath();
+            context.arc(x, y, 8, 0, Math.PI * 2, false);
+            context.closePath();
+            context.fillStyle = "#777";
+            context.fill();
+
+            context.fillStyle = "#efefef";
+            context.textAlign = "center";
+            context.fillText(atom.element, x, y+2);
+        }
+    }
+
+};
+
+var run = function() {
+
+    update(1/100);
+
+    draw();
+
+    setTimeout(function() { run(); }, 1/100);
+};
+
+run();
+
+
+/*
 // Matter.js module aliases
 var Engine = Matter.Engine,
     World = Matter.World,
@@ -34,6 +183,7 @@ engine.world.gravity.y = 0;
 
 // create two boxes and a ground
 Chem.load(6140, engine.world, $('body').width()/2, $('body').height()/2);
+*/
 
 /*
 var boxA = Bodies.rectangle(400, 200, 80, 80);
@@ -45,4 +195,4 @@ World.add(engine.world, [boxA, boxB, ground]);
 */
 
 // run the engine
-Engine.run(engine);
+//Engine.run(engine);
